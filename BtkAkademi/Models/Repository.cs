6b70113b892bc2fd2fd08b3eb8658067b1f30195
Omeryildiz08 <omer.yildiz27@hotmail.com>
2024-12-24 18:06:00 +0300

    repo work

---
+++ BtkAkademi/Models/Repository.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace BtkAkademi.Models
+{
+    public static class Repository
+    {
+        private static List<Candidate> applications = new();
+        public static IEnumerable<Candidate> Applications => applications;
+
+        public static void Add(Candidate candidate)
+        {
+            applications.Add(candidate);
+        }
+    }
+}
