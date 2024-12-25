9de227c183f948be9c01b75629202127d8c30c0a
Omeryildiz08 <omer.yildiz27@hotmail.com>
2024-12-25 20:21:09 +0300

    update v4

--- store/StoreApp/Controllers/ProductController.cs
+++ store/StoreApp/Controllers/ProductController.cs
@@ -7,7 +7,7 @@ namespace StoreApp.Controllers
 {
     public class ProductController : Controller
     {
-        
+
         private readonly RepositoryContext _context;
 
         public ProductController(RepositoryContext context)
@@ -15,10 +15,17 @@ namespace StoreApp.Controllers
             _context = context;
         }
 
-        public IActionResult Index(){
-        
-          var model = _context.Products.ToList();
-          return View(model);
+        public IActionResult Index()
+        {
+
+            var model = _context.Products.ToList();
+            return View(model);
+        }
+
+        public IActionResult Get(int id)
+        {
+            Product product = _context.Products.First(p => p.ProductId.Equals(id));
+            return View(product);
         }
     }
 }
